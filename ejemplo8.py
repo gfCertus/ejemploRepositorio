a6d3d2267da3eead71d79534976f0500da3c63cf
Docente Sede SJL <docente-sjl@certus.edu.pe>
2026-09-07 21:51:48 -0500

    ejemplo8 : archivo en bucle

--- ejemplo8.py
+++ ejemplo8.py
@@ -1,12 +1,17 @@
 # Modificar para que en cada iteracion inserte una nueva linea en un archivo.
 import time
 import keyboard
-
+archivo=open("archivoReto.txt","w")
+linea=0
 while True:
+    print("😈 ***\n")
+    archivo.write(f"{linea} ===> ********\n")
+    linea +=1
     if keyboard.is_pressed("esc"):
         print("\nTecla ESC detectada. Bucle detenido.")
+        
         break
-
     # Lógica de tu programa
     print("Procesando...", end="\r")
     time.sleep(0.1)
+archivo.close()
